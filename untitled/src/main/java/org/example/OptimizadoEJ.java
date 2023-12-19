035ebb092a1f0409b819a6a4743ede537ffeae35
danielaorrego3 <danielaorregoalfonso@gmail.com>
2023-12-19 01:55:57 -0500

    actu1.6

--- untitled/src/main/java/org/example/OptimizadoEJ.java
+++ untitled/src/main/java/org/example/OptimizadoEJ.java
@@ -3,101 +3,101 @@ package org.example;
 import java.io.IOException;
 
 public class OptimizadoEJ {
-    public static int[][] part2(int ha, int[] teams, int n) throws IOException
+    public static int[][] PTorneo(int ha, int[] equipos, int n) throws IOException
     {
 
-//nodes = the team that is on that node of the graph where nodes(1) is
-//always team n which has the smallest star length
-        int[] nodes = new int[n];
-        nodes[0]= teams[n-1];
+//nodos = el equipo que está en ese nodo del gráfico donde está nodos(1)
+//siempre el equipo n que tiene la longitud de estrella más pequeña
+        int[] nodos = new int[n];
+        nodos[0]= equipos[n-1];
 
-//Matching order of nodes with teams
+//Orden coincidente de nodos con equipos
         int a; int b=2;
         for (a=1; a<n; a++)
         {
             if (a<=n/2)
-                nodes[2*a-1]=teams[a-1];
+                nodos[2*a-1]=equipos[a-1];
             else
             {
-                nodes[b]=teams[a-1];
+                nodos[b]=equipos[a-1];
                 b=b+2;
             }
         }
 
-//Setting up a home-away vector (+1 or -1 values)
-        int[] havector = new int [n];
-        int number=-1; //number=-1 -> away; number=1 -> home
+//Configurar un vector de hogar lejos (valores +1 o -1)
+        int[] vector = new int [n];
+        int numero=-1; //número=-1 -> lejos; numero=1 -> casa
 
-//first half of home-away vector (first round)
-        for (a=0;a<((int)n/ha/2)*ha; a=a+ha) //CHANGED FROM n/ha  to int n/ha/2 * ha
+//primera mitad del vector de local-fuera (primera ronda)
+        for (a=0;a<((int)n/ha/2)*ha; a=a+ha) //CAMBIADO DE n/ha a int n/ha/2 * ha
         {
             for(b=0;b<ha;b++)
-                havector[a+b]=number;
-            number=number*-1;
+                vector[a+b]=numero;
+            numero=numero*-1;
         }
 
         double i = (Math.floor(n/(2*ha)))*ha;
         for(a=(int)i; a<n/2;a++)
-            havector[a]=number;
+            vector[a]=numero;
 
-//second half = negative mirror of first half (first round)
+//segunda mitad = espejo negativo de la primera mitad (primera ronda)
         for(a=0;a<n/2;a++)
-            havector[a+n/2]=-1*havector[n/2-a-1];
+            vector[a+n/2]=-1*vector[n/2-a-1];
 
-//setting up array for schedule
-        int[][] schedule = new int [n][2*n-2];
+//configurando una matriz para el horario
+        int[][] calendario = new int [n][2*n-2];
         int r; int k;
 
-//rounds in first half of tournaments
+//rondas en la primera mitad de los torneos
         for(r=0; r<n-1;r++)
         {
-            //for rounds>1, switch the home/away value on first node (if there has been
-            //a max # of home/away games in a row
+            //para rondas>1, cambie el valor local/visitante en el primer nodo (si ha habido
+            //un número máximo de partidos seguidos en casa/fuera
 
             if(r!=0 && r%ha==0)
             {
-                havector[0]=-1*havector[0];
-                havector[n-1]=-1*havector[n-1];
+                vector[0]=-1*vector[0];
+                vector[n-1]=-1*vector[n-1];
             }
 
-            //schedule for round r
+            //calendario para la ronda r
             k=n-1;
             for(a=0;a<n;a++)
             {
-                schedule[nodes[a]][r] = havector[a]*(1+nodes[k]);
+                calendario[nodos[a]][r] = vector[a]*(1+nodos[k]);
                 k=k-1;
             }
 
-            //Move all but the first node counter-clockwise one position
-            int temp = nodes[1];
+            //Mueva todos menos el primer nodo en el sentido contrario a las agujas del reloj una posición
+            int temp = nodos[1];
             for (a=1; a<n-1; a++)
-                nodes[a]=nodes[a+1];
-            nodes[n-1]=temp;
+                nodos[a]=nodos[a+1];
+            nodos[n-1]=temp;
         }
 
-//second half of tournament (opposite of round n-2, n-1, 1, 2, ..., n-3)
+//Segunda mitad del torneo (opuesta a la ronda n-2, n-1, 1, 2, ..., n-3)
         a=n-2;
         for(r=n-1;r<2*n-2;r++)
         {
-            for(b=0;b<n;b++) //b=team
-                schedule[b][r]=-1*schedule[b][a-1];
+            for(b=0;b<n;b++) //b=equipos
+                calendario[b][r]=-1*calendario[b][a-1];
             a=a+1;
             if (a==n)
                 a=1;
         }
 
 
-//printing/displaying resulting schedule
-        System.out.println("Team Schedule:");
+//imprimir/mostrar el cronograma resultante
+        System.out.println("horario del equipo::");
         for (a=0; a<n;a++){
             for(b=0;b<2*n-2;b++){
-                System.out.print(schedule[a][b]);
+                System.out.print(calendario[a][b]);
                 System.out.print("\t");
             }
             System.out.print("\n");
         }
         System.out.println();
-        return schedule;
+        return calendario;
 
     }
 }
